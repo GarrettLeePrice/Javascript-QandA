866131d963585ae37b0566d5e83b622e3c677f3c
garrett price <garrettleeprice@gmail.com>
2016-10-28 13:52:32 -0700

    add new answer form - new-answer component

---
+++ app/components/new-answer.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    saveAnswer() {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+      };
+      this.set('addNewAnswer', false);
+      this.sendAction('saveAnswer', params);
+    }
+  }
+});
