78688b1d80fbb681da3dea1c731fcc7e21a7d109
garrett price <garrettleeprice@gmail.com>
2016-10-28 12:08:20 -0700

    add dynamic routing

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,34 +1,14 @@
 import Ember from 'ember';
 
-
-
-
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
-
   actions: {
-    destroyQuestion(question) {
-      question.destroyRecord();
-      this.transitionTo('index');
-    },
-
     saveQuestion3(params) {
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
-    },
-
-    edit(question, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          question.set(key,params[key]);
-        }
-      });
-      question.save();
-      this.transitionTo('index');
     }
   }
-
 });
