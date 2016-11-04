c8c97b97d80a80a5d41cb6af043acf1ca24d059f
Garrett Price <garrettleeprice@gmail.com>
2016-11-04 09:58:42 -0700

    add service for favorite questions list

---
+++ app/components/display-favs.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  favQuestions: Ember.inject.service(),
+});
--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  favQuestions: Ember.inject.service(),
+  
   fullName: Ember.computed('question.author', 'question.authorlast', function() {
     return this.get('question.author') + " " + this.get('question.authorlast');
   }),
+
+  actions: {
+    addToFavs(question) {
+      this.get('favQuestions').add(question);
+    }
+  }
 
 });
