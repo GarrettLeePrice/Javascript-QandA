aeb44de7fda485b05ee47cbbc273551a3e8c2d23
garrett price <garrettleeprice@gmail.com>
2016-11-08 20:57:51 -0800

    add conditional statemnt to favorites service to check if favorited question is already in favorites list, and not allow it to be added twice.

--- app/services/fav-questions.js
+++ app/services/fav-questions.js
@@ -4,6 +4,10 @@ export default Ember.Service.extend({
   questions: [],
 
   add(question) {
+    if(this.get('questions').includes(question)) {
+      alert('already in favorites!');
+    }else{
     this.get('questions').pushObject(question);
   }
+}
 });
